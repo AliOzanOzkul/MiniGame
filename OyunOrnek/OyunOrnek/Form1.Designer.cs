cf0b50a4f2de0557823dd77308e1ed3e5032c352
AliOzanOzkul <aliozanozkul96@gmail.com>
2023-02-14 12:27:55 +0300

    Mini-Game

---
+++ OyunOrnek/OyunOrnek/Form1.Designer.cs
@@ -0,0 +1,190 @@
+﻿
+namespace OyunOrnek
+{
+    partial class AnaSayfa
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.lbl3d = new System.Windows.Forms.Label();
+            this.lbl1d = new System.Windows.Forms.Label();
+            this.lbl4d = new System.Windows.Forms.Label();
+            this.lbl2d = new System.Windows.Forms.Label();
+            this.lbl5d = new System.Windows.Forms.Label();
+            this.lbl7d = new System.Windows.Forms.Label();
+            this.lbl6d = new System.Windows.Forms.Label();
+            this.lblbitir = new System.Windows.Forms.Label();
+            this.pnlKutu = new System.Windows.Forms.Panel();
+            this.lblKazan = new System.Windows.Forms.Label();
+            this.lblKaybet = new System.Windows.Forms.Label();
+            this.SuspendLayout();
+            // 
+            // lbl3d
+            // 
+            this.lbl3d.BackColor = System.Drawing.Color.Black;
+            this.lbl3d.Dock = System.Windows.Forms.DockStyle.Top;
+            this.lbl3d.Location = new System.Drawing.Point(0, 0);
+            this.lbl3d.Name = "lbl3d";
+            this.lbl3d.Size = new System.Drawing.Size(800, 40);
+            this.lbl3d.TabIndex = 0;
+            this.lbl3d.Text = "label1";
+            // 
+            // lbl1d
+            // 
+            this.lbl1d.BackColor = System.Drawing.Color.Black;
+            this.lbl1d.Dock = System.Windows.Forms.DockStyle.Bottom;
+            this.lbl1d.Location = new System.Drawing.Point(0, 410);
+            this.lbl1d.Name = "lbl1d";
+            this.lbl1d.Size = new System.Drawing.Size(800, 40);
+            this.lbl1d.TabIndex = 1;
+            this.lbl1d.Text = "label2";
+            // 
+            // lbl4d
+            // 
+            this.lbl4d.BackColor = System.Drawing.Color.Black;
+            this.lbl4d.Dock = System.Windows.Forms.DockStyle.Right;
+            this.lbl4d.Location = new System.Drawing.Point(751, 40);
+            this.lbl4d.Name = "lbl4d";
+            this.lbl4d.Size = new System.Drawing.Size(49, 370);
+            this.lbl4d.TabIndex = 2;
+            this.lbl4d.Text = "label3";
+            // 
+            // lbl2d
+            // 
+            this.lbl2d.BackColor = System.Drawing.Color.Black;
+            this.lbl2d.Dock = System.Windows.Forms.DockStyle.Left;
+            this.lbl2d.Location = new System.Drawing.Point(0, 40);
+            this.lbl2d.Name = "lbl2d";
+            this.lbl2d.Size = new System.Drawing.Size(45, 370);
+            this.lbl2d.TabIndex = 3;
+            this.lbl2d.Text = "label4";
+            // 
+            // lbl5d
+            // 
+            this.lbl5d.BackColor = System.Drawing.Color.Black;
+            this.lbl5d.Location = new System.Drawing.Point(279, 230);
+            this.lbl5d.Name = "lbl5d";
+            this.lbl5d.Size = new System.Drawing.Size(49, 220);
+            this.lbl5d.TabIndex = 4;
+            this.lbl5d.Text = "label5";
+            // 
+            // lbl7d
+            // 
+            this.lbl7d.BackColor = System.Drawing.Color.Black;
+            this.lbl7d.Location = new System.Drawing.Point(501, 230);
+            this.lbl7d.Name = "lbl7d";
+            this.lbl7d.Size = new System.Drawing.Size(49, 220);
+            this.lbl7d.TabIndex = 5;
+            this.lbl7d.Text = "label6";
+            // 
+            // lbl6d
+            // 
+            this.lbl6d.BackColor = System.Drawing.Color.Black;
+            this.lbl6d.Location = new System.Drawing.Point(279, 188);
+            this.lbl6d.Name = "lbl6d";
+            this.lbl6d.Size = new System.Drawing.Size(271, 42);
+            this.lbl6d.TabIndex = 6;
+            this.lbl6d.Text = "label7";
+            // 
+            // lblbitir
+            // 
+            this.lblbitir.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(255)))), ((int)(((byte)(128)))), ((int)(((byte)(255)))));
+            this.lblbitir.Font = new System.Drawing.Font("Microsoft Sans Serif", 15F, System.Drawing.FontStyle.Bold, System.Drawing.GraphicsUnit.Point, ((byte)(162)));
+            this.lblbitir.ForeColor = System.Drawing.Color.SaddleBrown;
+            this.lblbitir.Location = new System.Drawing.Point(548, 308);
+            this.lblbitir.Name = "lblbitir";
+            this.lblbitir.Size = new System.Drawing.Size(206, 102);
+            this.lblbitir.TabIndex = 7;
+            this.lblbitir.Text = "Finish";
+            this.lblbitir.TextAlign = System.Drawing.ContentAlignment.MiddleCenter;
+            // 
+            // pnlKutu
+            // 
+            this.pnlKutu.BackColor = System.Drawing.Color.FromArgb(((int)(((byte)(192)))), ((int)(((byte)(192)))), ((int)(((byte)(0)))));
+            this.pnlKutu.Location = new System.Drawing.Point(106, 308);
+            this.pnlKutu.Name = "pnlKutu";
+            this.pnlKutu.Size = new System.Drawing.Size(103, 73);
+            this.pnlKutu.TabIndex = 8;
+            // 
+            // lblKazan
+            // 
+            this.lblKazan.AutoSize = true;
+            this.lblKazan.Location = new System.Drawing.Point(601, 60);
+            this.lblKazan.Name = "lblKazan";
+            this.lblKazan.Size = new System.Drawing.Size(13, 13);
+            this.lblKazan.TabIndex = 9;
+            this.lblKazan.Text = "0";
+            // 
+            // lblKaybet
+            // 
+            this.lblKaybet.AutoSize = true;
+            this.lblKaybet.Location = new System.Drawing.Point(697, 60);
+            this.lblKaybet.Name = "lblKaybet";
+            this.lblKaybet.Size = new System.Drawing.Size(13, 13);
+            this.lblKaybet.TabIndex = 10;
+            this.lblKaybet.Text = "0";
+            // 
+            // AnaSayfa
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(800, 450);
+            this.Controls.Add(this.lblKaybet);
+            this.Controls.Add(this.lblKazan);
+            this.Controls.Add(this.pnlKutu);
+            this.Controls.Add(this.lblbitir);
+            this.Controls.Add(this.lbl6d);
+            this.Controls.Add(this.lbl7d);
+            this.Controls.Add(this.lbl5d);
+            this.Controls.Add(this.lbl2d);
+            this.Controls.Add(this.lbl4d);
+            this.Controls.Add(this.lbl1d);
+            this.Controls.Add(this.lbl3d);
+            this.Name = "AnaSayfa";
+            this.StartPosition = System.Windows.Forms.FormStartPosition.CenterScreen;
+            this.Text = "Kutu Oyunu";
+            this.KeyDown += new System.Windows.Forms.KeyEventHandler(this.AnaSayfa_KeyDown);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Label lbl3d;
+        private System.Windows.Forms.Label lbl1d;
+        private System.Windows.Forms.Label lbl4d;
+        private System.Windows.Forms.Label lbl2d;
+        private System.Windows.Forms.Label lbl5d;
+        private System.Windows.Forms.Label lbl7d;
+        private System.Windows.Forms.Label lbl6d;
+        private System.Windows.Forms.Label lblbitir;
+        private System.Windows.Forms.Panel pnlKutu;
+        private System.Windows.Forms.Label lblKazan;
+        private System.Windows.Forms.Label lblKaybet;
+    }
+}
+
---
+++ OyunOrnek/OyunOrnek/Form1.cs
@@ -0,0 +1,129 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows.Forms;
+
+namespace OyunOrnek
+{
+    public partial class AnaSayfa : Form
+    {
+        public AnaSayfa()
+        {
+            InitializeComponent();
+        }
+
+        private void AnaSayfa_KeyDown(object sender, KeyEventArgs e)
+        {
+            Keys key = e.KeyCode;
+
+            switch (key)
+            {
+                case Keys.Up:
+                    YukarıCık();
+                    break;
+                case Keys.Down:
+                    AsagiIn();
+                    break; 
+                case Keys.Right:
+                    SagaGit();
+                    break;
+                case Keys.Left:
+                    SolaGit();
+                    break;
+                case Keys.W:
+                    YukarıCık();
+                    break;
+                case Keys.S:
+                    AsagiIn();
+                    break;
+                case Keys.A:
+                    SolaGit();
+                    break;
+                case Keys.D:
+                    SagaGit();
+                    break;
+
+                default:
+                    break;
+            }
+            OyunBittiMi();
+        }
+
+        int kaybet;
+        int kazan;
+        private void OyunBittiMi()
+        {
+
+            bool kazandiMi = pnlKutu.Bounds.IntersectsWith(lblbitir.Bounds);
+            bool kaybettiMi = pnlKutu.Bounds.IntersectsWith(lbl1d.Bounds)
+                || pnlKutu.Bounds.IntersectsWith(lbl2d.Bounds)
+                || pnlKutu.Bounds.IntersectsWith(lbl3d.Bounds)
+                || pnlKutu.Bounds.IntersectsWith(lbl4d.Bounds)
+                || pnlKutu.Bounds.IntersectsWith(lbl5d.Bounds)
+                || pnlKutu.Bounds.IntersectsWith(lbl6d.Bounds)
+                || pnlKutu.Bounds.IntersectsWith(lbl7d.Bounds);
+                
+            if(kazandiMi)
+            {
+
+                kazan++;
+                DialogResult dr = MessageBox.Show("Tekrar deneyiniz", "Kazandınız!", MessageBoxButtons.YesNo);
+
+                if (dr == DialogResult.Yes)
+                {
+                    pnlKutu.Location = new Point(106, 308);
+                }
+                else
+                {
+                    Application.Exit();
+                }
+
+            }
+            
+            if (kaybettiMi)
+            {
+
+                kaybet++;
+                DialogResult dr = MessageBox.Show("Tekrar deneyiniz", "Kaybettiniz!", MessageBoxButtons.YesNo);
+                
+                lblKaybet.Text = (kaybet).ToString();
+
+                if (dr == DialogResult.Yes)
+                {
+                    pnlKutu.Location = new Point(106, 308);
+                }
+                else
+                {
+                    Application.Exit();
+                }
+
+            }
+
+
+        }
+        private void SolaGit()
+        {
+            pnlKutu.Left -= 10;
+        }
+
+        private void SagaGit()
+        {
+            pnlKutu.Left += 10;
+        }
+
+        private void AsagiIn()
+        {
+            pnlKutu.Top += 10;
+        }
+
+        private void YukarıCık()
+        {
+            pnlKutu.Top -= 10;
+        }
+    }
+}
